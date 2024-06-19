396257cabd85d879e47517802a0eb72daf2bfa2d
Reem Sakr <reemsakr060@gmail.com>
2024-06-20 01:08:07 +0300

    create config-server service

---
+++ services/config-server/src/main/java/gov/iti/jets/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package gov.iti.jets.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
